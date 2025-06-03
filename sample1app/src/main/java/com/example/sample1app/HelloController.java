f183bfdddfe99bd8a0b666a759618161a3851522
naoyaKobayasi <117634227+naoyaKobayasi@users.noreply.github.com>
2025-06-03 16:05:57 +0900

    「フォームを利用する」まで

--- sample1app/src/main/java/com/example/sample1app/HelloController.java
+++ sample1app/src/main/java/com/example/sample1app/HelloController.java
@@ -1,17 +1,26 @@
 package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/{num}")
-    public ModelAndView index(ModelAndView mav,@PathVariable int num){
-        mav.addObject("msg","This message in Controller.");
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView mav){
+        mav.addObject("msg","名前を書いてください。");
+        mav.setViewName("index");
+        return mav;
+    }
+
+    @RequestMapping(value = "/",method = RequestMethod.POST)
+    public ModelAndView form(@RequestParam("text1") String str,ModelAndView mav){
+        mav.addObject("msg","こんにちは、"+str+"さん");
+        mav.addObject("value", str);
         mav.setViewName("index");
         return mav;
     }
