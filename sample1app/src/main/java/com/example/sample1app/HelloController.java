bcfead4bb7e52406e480d0e3ecca66ee96f4bc35
naoyaKobayasi <117634227+naoyaKobayasi@users.noreply.github.com>
2025-06-03 14:24:27 +0900

    ModelAndView実装

--- sample1app/src/main/java/com/example/sample1app/HelloController.java
+++ sample1app/src/main/java/com/example/sample1app/HelloController.java
@@ -3,17 +3,17 @@ package com.example.sample1app;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/{temp}")
-    public String index(@PathVariable String temp) {
-        switch (temp) {
-            case "index":
-                return "index";
-            default:
-                return "other";
-        }
+    @RequestMapping("/{num}")
+    public ModelAndView index(ModelAndView mav,@PathVariable int num){
+        mav.addObject("msg","This message in Controller.");
+        mav.setViewName("index");
+        return mav;
     }
+    
 }
