54c5dc3c1000d0458ddd223008f247d274ee3c4c
naoyaKobayasi <117634227+naoyaKobayasi@users.noreply.github.com>
2025-06-10 13:59:17 +0900

    th:ifまで

--- sample1app/src/main/java/com/example/sample1app/HelloController.java
+++ sample1app/src/main/java/com/example/sample1app/HelloController.java
@@ -2,7 +2,6 @@ package com.example.sample1app;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,18 +9,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HelloController {
 
+    private boolean flag = false;
+
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public ModelAndView index(ModelAndView mav){
-        mav.addObject("msg","名前を書いてください。");
+        flag = !flag;
         mav.setViewName("index");
-        return mav;
-    }
-
-    @RequestMapping(value = "/",method = RequestMethod.POST)
-    public ModelAndView form(@RequestParam("text1") String str,ModelAndView mav){
-        mav.addObject("msg","こんにちは、"+str+"さん");
-        mav.addObject("value", str);
-        mav.setViewName("index");
+        mav.addObject("flag",flag);
+        mav.addObject("msg", "サンプルメッセージです。");
         return mav;
     }
     
